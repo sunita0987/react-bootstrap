f7d9139ac88c93a6b602289d103a6cfce08f2114
sunita0987 <sunita0987546@gmail.com>
2025-04-02 18:49:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <p className='fw-bold'>hello</p>
+    </div>
+  )
+}
+
+export default App
